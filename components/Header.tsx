6c5bd2a83825776492f6997a645c2cccdeb65f25
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2023-11-16 23:37:34 +0200

    0.2(fixed button bugs)

--- components/Header.tsx
+++ components/Header.tsx
@@ -104,6 +104,7 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button
+                        onClick={() => router.push('/')} 
                         className="
                             rounded-full
                             p-2
@@ -117,6 +118,7 @@ const Header: React.FC<HeaderProps> = ({
                         <HiHome className='text-black' size={20}/>
                     </button>
                     <button
+                        onClick={() => router.push('/search')} 
                         className="
                             rounded-full
                             p-2
